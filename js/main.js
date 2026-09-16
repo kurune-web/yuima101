6a91b2dad27b3ed3d8a53af426469540bd81da08
kurune-web <kurune.art@gmail.com>
2026-09-16 12:45:34 +0900

    NEWS記事ページ作成・リンク調整

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ const header = document.querySelector(".header");
 window.addEventListener("scroll", () => {
   header.classList.toggle("scroll-nav", window.scrollY > 100);
 });
+
+
+const navInput = document.querySelector("#nav-input");
+const navLinks = document.querySelectorAll(".nav_drawer_list a[href^='#']");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (navInput) {
+      navInput.checked = false;
+    }
+  });
+});
